84c253c843b34ebfff6a00cc9d34a19414ce1d8d
Nathan Park <nyp5aa@virginia.edu>
2018-06-06 11:43:55 -0400

    created new user button, firebase auth is adding

--- pomodoro/src/components/LoginForm.js
+++ pomodoro/src/components/LoginForm.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import firebase from 'firebase'
-
-const config = {
-    apiKey: "AIzaSyBwArX-pn0ctOAs2LGMJPPUgf4AVBD2jrc",
-    authDomain: "pomodorotimerr.firebaseapp.com",
-    databaseURL: "https://pomodorotimerr.firebaseio.com",
-    projectId: "pomodorotimerr",
-    storageBucket: "",
-    messagingSenderId: "455549070062"
-  };
-firebase.initializeApp(config);
+import { Link } from 'react-router-dom';
+import firebase from 'firebase';
+import config from './firebaseInfo';
 
 class LoginForm extends Component {
     render(){
--- pomodoro/src/components/NewUserForm.js
+++ pomodoro/src/components/NewUserForm.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import config from './firebaseInfo';
+
+firebase.initializeApp(config);
 
 class NewUserForm extends Component {
+
+  createNewUser=()=>{
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+    firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+    });
+  }
+
   render() {
     return (
       <div>
-        Hello World!
+        <div>
+          Enter your E-mail:
+          <input id="user"/>
+        </div>
+        <div>
+          Create a Password
+          <input id="pass"/>
+        </div>
+        <div>
+          <button onClick={this.createNewUser}>
+            Create
+          </button>
+        </div>
       </div>
     );
   }
